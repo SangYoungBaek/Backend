3c3a9c97c1a5ce2dd8f0093b2377f14a343f2687
heypop <hey@herenas.com>
2023-10-11 22:40:25 +0900

    Refactor: AWS S3 수정

---
+++ src/main/java/com/starta/project/global/aws/AmazonS3Service.java
@@ -0,0 +1,84 @@
+package com.starta.project.global.aws;
+
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AmazonS3Service {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String S3Bucket;
+
+    private final AmazonS3 amazonS3Client;
+
+    // 단일 이미지 업로드
+    public String upload(MultipartFile image) throws IOException {
+
+        String fileName = generateFileName(image);
+        long size = image.getSize();
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(image.getContentType());
+        objectMetadata.setContentLength(size);
+
+        amazonS3Client.putObject(
+                new PutObjectRequest(S3Bucket, fileName, image.getInputStream(), objectMetadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead)
+        );
+        return URLDecoder.decode(amazonS3Client.getUrl(S3Bucket, fileName).toString(), "utf-8");
+    }
+
+    // 다중 이미지 업로드
+    public ResponseEntity<Object> listUpload(MultipartFile[] multipartFileList) throws Exception {
+        List<String> imagePathList = new ArrayList<>();
+
+        for(MultipartFile multipartFile: multipartFileList) {
+            String originalName = multipartFile.getOriginalFilename(); // 파일 이름
+            long size = multipartFile.getSize(); // 파일 크기
+
+            ObjectMetadata objectMetaData = new ObjectMetadata();
+            objectMetaData.setContentType(multipartFile.getContentType());
+            objectMetaData.setContentLength(size);
+
+            // S3에 업로드
+            amazonS3Client.putObject(
+                    new PutObjectRequest(S3Bucket, originalName, multipartFile.getInputStream(), objectMetaData)
+                            .withCannedAcl(CannedAccessControlList.PublicRead)
+            );
+
+            String imagePath = URLDecoder.decode(amazonS3Client.getUrl(S3Bucket, originalName).toString(),"utf-8"); // 접근가능한 URL 가져오기
+            imagePathList.add(imagePath);
+        }
+        return new ResponseEntity<Object>(imagePathList, HttpStatus.OK);
+    }
+    // 이미지 삭제
+    public void deleteFile(String fileName) throws IOException {
+        try {
+            amazonS3Client.deleteObject(S3Bucket, fileName);
+        } catch (SdkClientException e) {
+            throw new IOException("Error deleting file from S3", e);
+        }
+    }
+    // 파일 이름 생성
+    private String generateFileName(MultipartFile file) {
+        return UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+    }
+}
