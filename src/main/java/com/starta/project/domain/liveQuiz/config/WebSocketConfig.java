3a86f5d1130a43996c2ca09c3b6f9c83d13853c5
heypop <hey@herenas.com>
2023-11-08 03:11:56 +0900

    Marge: dev > main - 코드 컨벤션에 맞게 진행 (app, topic)

--- src/main/java/com/starta/project/domain/liveQuiz/config/WebSocketConfig.java
+++ src/main/java/com/starta/project/domain/liveQuiz/config/WebSocketConfig.java
@@ -12,12 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     // 메시지 브로커가 /topic으로 시작하는 메시지가 메시지를 구독하는 클라이언트에게 라우팅 되도록 정의
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/api/chat"); // 메시지 브로커에 대한 Prefix 설정
-        config.setApplicationDestinationPrefixes("/api/send"); // 클라이언트에서 메시지 송신 시 prefix 설정
+        config.enableSimpleBroker("/topic"); // 메시지 브로커에 대한 Prefix 설정
+        config.setApplicationDestinationPrefixes("/app"); // 클라이언트에서 메시지 송신 시 prefix 설정
     }
     // topic으로 시작하는 메시지가 message-handling methods로 라우팅 되어야 한다는 것을 정의
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/ws");
+        registry.addEndpoint("/api/ws").setAllowedOrigins("*");
     }
 }
--- src/main/java/com/starta/project/domain/liveQuiz/controller/LiveQuizController.java
+++ src/main/java/com/starta/project/domain/liveQuiz/controller/LiveQuizController.java
@@ -17,9 +17,8 @@ public class LiveQuizController {
 
     private final LiveQuizService liveQuizService;
 
-    // 클라이언트가 /api/sendMassage로 메시지를 보내면, 서버는 /topic/liveQuizChatRoom로 메시지를 보낸다.
-    @MessageMapping("/liveSendMassage")
-    @SendTo("/api/chat/liveChatRoom")
+    @MessageMapping("/liveChatRoom") //app/liveSendMassage
+    @SendTo("/topic/liveChatRoom")
     public ChatMessageDto sendMessage(
             @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails,
             ChatMessageDto chatMessage) {
